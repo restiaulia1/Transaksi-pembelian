ec0e5d004194a3751fbb752673ad2a00ed229adc
Resti <restiaulia480@gmail.com>
2025-02-24 08:42:47 +0700

    mengubah event driven link hapus

--- main.js
+++ main.js
@@ -121,3 +121,8 @@ export async function ambilDaftarBarangDiKeranjang() {
 
   return hasilKueri;
 }
+
+ export async function hapusBarangDariKeranjang(id) {
+   await deleteDoc(doc(basisdata, "transaksi", id))
+ }
+ 
